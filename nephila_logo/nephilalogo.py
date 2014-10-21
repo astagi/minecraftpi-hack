3be33b5e363cd86931f242d9ff46b16be44eabdb
Andrea Stagi <stagi.andrea@gmail.com>
2014-10-22 01:36:48 +0200

    Added Nephila logo generation on Minecraft

--- nephila_logo/nephilalogo.py
+++ nephila_logo/nephilalogo.py
@@ -1,4 +1,10 @@
 from PIL import Image
+from mcpi import minecraft, block
+
+mc = minecraft.Minecraft.create()
+mc.postToChat('Nephila logo demo')
+
+player_pos = mc.player.getPos()
 
 im = Image.open('nephilalogo.png')
 pixels = im.load()
@@ -6,4 +12,9 @@ pixels = im.load()
 for i in range(im.size[0]):
     for j in range(im.size[1]):
         pixel = pixels[i,j]
-
+        if pixel[0] > 100:
+            type = block.STONE
+        else:
+            type = block.GLASS
+        mc.setBlock(player_pos.x + 2 , player_pos.y - j, player_pos.z + i, type)        
+        print pixel
